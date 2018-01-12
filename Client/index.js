a578a1c3c941184aa541bc4dfcf3df6ce2df6053
DRL\preecjo <jon.preece@ao.com>
2018-01-12 10:38:09 +0000

    Tidy up based on feedback from MC

--- Client/index.js
+++ Client/index.js
@@ -1,43 +1,39 @@
 import './traffic-light'
 
-(() => {
-  'use strict'
+const onTrafficLightsChanged = currentState => {
+  const event = new CustomEvent('TrafficLightChanged', {
+    detail: {
+      currentState
+    }
+  })
+  window.dispatchEvent(event)
+}
 
-  const onTrafficLightsChanged = currentState => {
-    const event = new CustomEvent('TrafficLightChanged', {
-      detail: {
-        currentState
-      }
-    })
-    window.dispatchEvent(event)
-  }
+const lights = {
+  possibleState: ['red', 'amber', 'green'],
+  currentState: 'red'
+}
 
-  let lights = {
-    possibleState: ['red', 'amber', 'green'],
-    currentState: 'red'
-  }
-
-  const handler = {
-    get (target, name) {
-      if (name === 'currentState') {
-        return target[name]
-      }
-      console.warn(`Property "${name}" is not available`)
-    },
-    set (obj, prop, value) {
-      const found = lights.possibleState.findIndex(x => x === value) > -1
-      if (!found) {
-        console.warn(`Could not find "${value}"`)
-        return true
-      }
-      obj[prop] = value
-      onTrafficLightsChanged(value)
+const handler = {
+  get (target, name) {
+    if (name === 'currentState') {
+      return target[name]
+    }
+    console.warn(`Property "${name}" is not available`)
+  },
+  set (obj, prop, value) {
+    const found = lights.possibleState.findIndex(x => x === value) > -1
+    if (!found) {
+      console.warn(`Could not find "${value}"`)
       return true
     }
+    obj[prop] = value
+    onTrafficLightsChanged(value)
+    return true
   }
+}
 
-  const proxy = new Proxy(lights, handler)
-  proxy.currentState = 'red'
+const proxy = new Proxy(lights, handler)
+window.trafficLights = proxy
 
-  window.trafficLights = proxy
-})()
+onTrafficLightsChanged(proxy.currentState)
--- Client/traffic-light.js
+++ Client/traffic-light.js
@@ -2,14 +2,8 @@ export default class TrafficLight extends HTMLElement {
   static get selector () { return 'traffic-light' }
 
   connectedCallback () {
-    this._root = this
-
     window.addEventListener('TrafficLightChanged', event => {
-      this._root.innerHTML = ''
-      const span = document.createElement('span')
-      span.innerText = event.detail.currentState
-
-      this._root.appendChild(span)
+      this.textContent = event.detail.currentState
     })
   }
 }
